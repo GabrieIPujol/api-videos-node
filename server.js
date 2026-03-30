08121ee3e3995f90285145419f67302c82fc5ae7
GabrieIPujol <gabriel.pagvelosa@gmail.com>
2026-03-30 14:25:09 -0300

    Finalizando a API com CRUD completo de videos

--- server.js
+++ server.js
@@ -9,25 +9,49 @@
 // server.listen(3333)
 
 import { fastify } from 'fastify'
+// import { DatabaseMemory } from './database-memory.js'
+import { DatabasePostgress } from './database-postgres.js'
 
 const server = fastify()
+const database = new DatabasePostgress()
 
-server.post('/videos', () => {
-    return 'Cria video'
+server.post('/videos', async(request, reply) => {
+
+    const { title, description, duration } = request.body
+
+    await database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(201).send()
 })
 
-server.get('/videos', () => {
-    return 'Visualiza o video'
+server.get('/videos', async(request) => {
+    const search = request.query.search
+    const videos = await database.list(search)
+    return videos
 })
 
-server.put('/videos/:id', () => {
-    return 'Edita o video'
+server.put('/videos/:id', async(request, reply) => {
+    const videoId = request.params.id
+    const { title, description, duration } = request.body
+
+    await database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+    return reply.status(204).send()
 })
 
-server.delete('/videos/:id', () => {
-    return 'Deleta video'
+server.delete('/videos/:id', async(request, reply) => {
+    const videoId = request.params.id
+    await database.delete(videoId)
+    return reply.status(204).send()
 })
 
 server.listen({
-    port: 3333,
+    port: process.env.PORT ?? 3333,
 })
